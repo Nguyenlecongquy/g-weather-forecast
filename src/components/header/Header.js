fcd014729b05e6c0f25d50eaf3ee152aaf082cc3
Nguyen Le Cong Quy <nguyenlecongquy@gmail.com>
2024-08-06 17:26:43 +0700

    config tailwind and create Header

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-blue-500 py-3">
+      <span className="text-3xl text-white text-center py-4 font-bold">
+        Weather Dashboard
+      </span>
+    </div>
+  );
+};
+
+export default Header;
