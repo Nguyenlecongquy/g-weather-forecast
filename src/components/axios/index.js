105d56954c1dda1da945620b00e5375d1af4a1b1
Nguyen Le Cong Quy <nguyenlecongquy@gmail.com>
2024-08-07 17:15:47 +0700

    fix to build app

--- src/components/axios/index.js
+++ src/components/axios/index.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-// import * as dotenv from "dotenv";
-// dotenv.config();
 
 const instance = axios.create({
   baseURL: "https://g-weather-forecast-backend-4y3c.onrender.com/v1",
